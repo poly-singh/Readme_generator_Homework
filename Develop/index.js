4a666ec1017897adbce68d52e06b6abee42080ab
poly singh <poly.singh11@gmail.com>
2021-05-08 07:54:56 -0700

    adding index file

---
+++ Develop/index.js
@@ -0,0 +1,104 @@
+// TODO: Include packages needed for this application
+const inquirer=require('inquirer');
+const fs=require('fs');
+const axios = require("axios");
+const generate=require('./utils/generateMarkdown.js');
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+    // License Options
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+];
+inquirer
+.prompt(questions)
+.then(function(data){
+    const queryUrl = `https://api.github.com/users/${data.username}`;
+
+    axios.get(queryUrl).then(function(res) {
+        
+        const githubInfo = {
+            githubImage: res.data.avatar_url,
+            email: res.data.email,
+            profile: res.data.html_url,
+            name: res.data.name
+        };
+        
+      fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+        if (err) {
+          throw err;
+        };
+
+        console.log("New README file created with success!");
+      });
+    });
+
+});
+
+
+function init() {
+
+}
+init();
+
+
+
